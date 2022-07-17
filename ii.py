e508dfa200459f615bf35bdfdacf21ffe39cb93f
Vasily Gavrilov <rubgavrilov@gmail.com>
2022-07-17 12:04:36 +0700

    Update file

--- ii.py
+++ ii.py
@@ -4,10 +4,11 @@
 __author__ = 'Гаврилов Василий 91840'
 
 from openpyxl import load_workbook
+import openpyxl as opx
 import pymysql
 
 print("Откреывается dataset")
-wb = load_workbook('./datas/dataset.xlsx',read_only=True)
+wb = load_workbook('./datas/dataset.xlsx') #,read_only=True
 sheet_array = wb.sheetnames
 #print(sheet_array[0])
 #sheet = wb[sheet_array[0]]
@@ -23,16 +24,12 @@ print("Столбцов: ", cols)
 print("start ii")
 i=0
 #tnvdId = dict({})
-t = {}
-g = {}
-for row in ws:
-    i+=1
-    if i==1:
-        continue
-    idDTnvd = row[1].value
-    idDTehr = row[2].value
-    idDGroup = row[3].value
+error = 0
+ws.insert_cols(11,2)
 
+ws[1][10].value = "Ошибка! Иностранная организация должна иметь ТН ВЭД"
+ws[1][10].font = opx.styles.Font(color='ff0000')
+def secondFast(t,idDTnvd,idDTehr):
     try:
         codes = idDTnvd.split(';')
     except Exception:
@@ -65,41 +62,69 @@ for row in ws:
                 except KeyError:
                     t[code] = {}
                     t[code][idDTehr] = 1
+    return t
+
+TnvdTehreg = {}
+TnvdGroup = {}
+TnvdNameprod = {}
+TehregTnvd = {}
+for row in ws:
+    i+=1
+    if i==1:
+        continue
+    idDTnvd = row[1].value
+    idDTehr = row[2].value
+    idDGroup = row[3].value
+    idDName = row[4].value
+    idDCity = row[9].value
+
+    if(idDCity!="РОССИЯ")and(idDTnvd is None):
+        # print(type(idDTnvd),idDTnvd)
+        # print(idDCity)
+        row[10].value = 1
+        row[10].font = opx.styles.Font(color='ff0000')
+
+        error+=1
+
+    try:
+        idDTehrs = idDTehr.split(';')
+        for idDTehr in idDTehrs:
+            idDTehr = str(idDTehr).strip()
+            TnvdTehreg = secondFast(TnvdTehreg,idDTnvd,idDTehr)
+    except Exception:
+        idDTehr = str(idDTehr).strip()
+        TnvdTehreg = secondFast(TnvdTehreg,idDTnvd,idDTehr)
+    #
+    try:
+        idDGroups = idDGroup.split(';')
+        for idDGroup in idDGroups:
+            idDGroup = str(idDGroup).strip()
+            TnvdGroup = secondFast(TnvdGroup,idDTnvd,idDGroup)
+    except Exception:
+        idDGroup = str(idDGroup).strip()
+        TnvdGroup = secondFast(TnvdGroup,idDTnvd,idDGroup)
+
+    try:
+        idDNames = idDName.split(';')
+        for idDName in idDNames:
+            idDName = str(idDName).strip()
+            TnvdNameprod = secondFast(TnvdNameprod,idDTnvd,idDName)
+    except Exception:
+        idDName = str(idDName).strip()
+        TnvdNameprod = secondFast(TnvdNameprod,idDTnvd,idDName)
 
 
 
     try:
-        groups = idDGroup.split(';')
+        idDTnvds = idDTnvd.split(';')
+        for idDTnvd in idDTnvds:
+            idDTehr = str(idDTehr).strip()
+            idDTnvd = str(idDTnvd).strip()
+            TehregTnvd = secondFast(TehregTnvd,idDTehr,idDTnvd)
     except Exception:
-        groups = idDGroup
-    if(isinstance(groups, int)==True):
-        groups = str(groups).strip()
-        try:
-            if len(g[groups])>0:
-                g[groups][idDTehr] += 1
-        except KeyError:
-            try:
-                if len(g[groups])>0:
-                    g[groups][idDTehr] = 1
-            except KeyError:
-                g[groups] = {}
-                g[groups][idDTehr] = 1
-    if(isinstance(groups, list)==True):
-        for x in groups:
-            if groups.count(x) > 1:
-                groups.remove(x)
-        for group in groups:
-            group = str(group).strip()
-            try:
-                if len(g[group])>0:
-                    g[group][idDTehr] += 1
-            except KeyError:
-                try:
-                    if len(g[group])>0:
-                        g[group][idDTehr] = 1
-                except KeyError:
-                    g[group] = {}
-                    g[group][idDTehr] = 1
+        idDTehr = str(idDTehr).strip()
+        idDTnvd = str(idDTnvd).strip()
+        TehregTnvd = secondFast(TehregTnvd,idDTehr,idDTnvd)
 
 
     if i%3000==1:
@@ -116,7 +141,17 @@ for row in ws:
     # country = ws[row][9].value
     # manufacturerAddress = ws[row][10].value
 print("end")
-print("Кодов ТНВД",len(t))
-print("9503004900: ",t["9503004900"])
-'ТР ЕАЭС 043/2017 О требованиях к средствам обеспечения пожарной безопасности и пожаротушения'
-print(g["Игрушки прочие"])
+print("Ошибок по стране без ТНВЭД",error)
+print("Кодов ТНВД",len(TnvdTehreg))
+print("9503004900 TnvdTehreg: ",TnvdTehreg["9503004900"])
+print("Групп товаров",len(TnvdGroup))
+print("9503004900 TnvdGroup: ",TnvdGroup["9503004900"])
+# print("9503004900 TnvdNameprod: ",TnvdNameprod["9503004900"])
+print("9503004900 TehregTnvd: ",TehregTnvd["ТР ТС 008/2011 О безопасности игрушек"])
+
+print("950300100 TnvdNameprod: ",TnvdNameprod["950300100"])
+
+# 'ТР ЕАЭС 043/2017 О требованиях к средствам обеспечения пожарной безопасности и пожаротушения'
+# ws.template = False , as_template=False
+wb.save('./datas/document.xlsx')
+print("Сохренен!")
